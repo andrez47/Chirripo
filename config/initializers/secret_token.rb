f09b42fe3b8f72aab156040b4d01357c594882bf
Andres Hernandez <andres@appcoast.com>
2012-09-08 00:10:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chirripo::Application.config.secret_token = '980bc3216059503c67a1c832dbc6f5e2ce90cab4630ab5bdcd21aff9bc2d1d975e1c1db7dfc386f17d3d31a5f3ba5bb9a2f98ea3607483d9928f4f43a188b371'
